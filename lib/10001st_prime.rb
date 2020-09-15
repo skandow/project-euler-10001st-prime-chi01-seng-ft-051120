7ff04667a8654094a083ba1b4a0a69ae68ce386b
skandow <kando1sr@gmail.com>
2020-09-15 15:08:37 -0400

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,20 @@
 # Implement your procedural solution here!
+
+def prime_number_for(nth_element)
+    counter = 1 
+    integer = 2
+    while counter != nth_element
+        integer += 1
+        if is_prime(integer)
+            counter += 1
+        end 
+    end 
+    integer   
+end
+
+def is_prime(number)
+    (2..(number - 1)).each do |num|
+        return false if number % num == 0 
+    end 
+    true 
+end 
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,29 @@
 # Implement your object-oriented solution here!
+
+class Prime 
+
+    attr_reader :value
+
+    def initialize(value)
+        @value = value
+    end 
+
+    def number
+        counter = 1 
+        integer = 2
+        while counter != self.value
+            integer += 1
+            if is_prime(integer)
+                counter += 1
+            end 
+        end 
+        integer   
+    end 
+
+    def is_prime(number)
+        (2..(number - 1)).each do |num|
+            return false if number % num == 0 
+        end 
+        true 
+    end 
+end 
